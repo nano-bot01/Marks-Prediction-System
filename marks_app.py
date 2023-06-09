3479fce7496fef3cc694f7c11a880bf4dd93c59e
Ankit Nainwal <78251168+nano-bot01@users.noreply.github.com>
2023-06-09 20:32:13 +0530

    Update marks_app.py

--- marks_app.py
+++ marks_app.py
@@ -56,7 +56,7 @@ def main():
     
     # getting the input data from input user
     
-    Hours = st.text_input("Enter the number of hour of actually study (range between 0-13) ")
+    Hours = st.text_input("Enter the number of hour of actually study (range between 1-13) ")
     
     
     # code for prediction 
